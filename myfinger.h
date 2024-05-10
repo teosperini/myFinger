1c0d77f6c1b5a367dc9bedf4fe3ebfd0e54bc73b
teosperini <85560128+teosperini@users.noreply.github.com>
2024-05-10 12:03:46 +0200

    Update myfinger.h

--- myfinger.h
+++ myfinger.h
@@ -13,17 +13,21 @@
 #include <sys/stat.h>
 #include <limits.h>
 
+#define MAX_USERS 100  // Numero massimo di utenti
+#define MAX_NAME_LENGTH 32  // Lunghezza massima del nome utente
+
 void getActiveUsers();
 void getSpecifiedUser(const char* user);
 void getSpecifiedAll();
 char* formatTime(const time_t login_time, bool login);
 void printSpecificUTMP(const struct utmp* ut);
+void printSpecificPW(const struct passwd* pw);
 char* formatPhoneNumber(const char* phoneNumber);
-void getSpecifiedUserPW(const char* user);
 bool checkAsterisk(const char* line);
 void printShort(const struct passwd* pw, const struct utmp* ut);
 void printLong(const struct passwd* pw, const struct utmp* ut);
-void printInit(const struct passwd* pw);
+void printStartL(const struct passwd* pw);
+void printEndL(const struct passwd* pw);
 void handle_no_names();
 void handle_names(char** names, int names_count);
 
